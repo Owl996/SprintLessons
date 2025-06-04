5be0204cbe390522ea5bb39813b80dc201f82628
dlironov <dlironov@bk.ru>
2025-06-04 23:53:49 +0300

    KS-1-2 create

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,16 @@
+package lesson_1
+
+fun main() {
+
+    var ordersCount: Long = 75
+    val purchaseMessage: String = "Thanks for the purchase"
+
+    println(ordersCount)
+    println(purchaseMessage)
+
+//    var employees: Int = 2000
+//    println(employees)
+
+    var employees: Int = 1999
+    println(employees)
+}

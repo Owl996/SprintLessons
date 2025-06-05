60c9c3077f3776c9f56c07cfede20190244b47b8
dlironov <dlironov@bk.ru>
2025-06-05 21:50:07 +0300

    KS-2-4-arithmetic updated

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,6 +1,6 @@
 package lesson_2
 
-const val SUPER_BUFF = 120
+const val SUPER_BUFF = 20
 const val PERCENTAGE_DIVISOR = 100
 
 fun main() {
@@ -11,6 +11,6 @@ fun main() {
     val crystalsWithBuff = (crystals * SUPER_BUFF / PERCENTAGE_DIVISOR)
     val ironOreWithBuff = (ironOre * SUPER_BUFF / PERCENTAGE_DIVISOR)
 
-    println("Crystals with buff $crystalsWithBuff")
-    println("Metal ore with buff  $ironOreWithBuff")
+    println("Crystal ore bonus $crystalsWithBuff")
+    println("Metal ore bonus  $ironOreWithBuff")
 }

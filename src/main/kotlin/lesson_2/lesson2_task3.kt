4a8416b940848f4a67519c7ca1bf98c67e9a9347
dlironov <dlironov@bk.ru>
2025-06-05 21:18:20 +0300

    KS-2-3-arithmetic created

---
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -0,0 +1,24 @@
+package lesson_2
+
+const val MINUTES_IN_HOUR = 60
+
+fun main() {
+
+    val departureHour = 9
+    val departureMinute = 39
+    val travelTimeMinutes = 457
+
+    // Время начала пути в минутах от полуночи
+    val departureTimeMinutes = departureHour * MINUTES_IN_HOUR + departureMinute
+
+    // Время прибытия в минутах от начала пути
+    val arrivalTimeMinutes = departureTimeMinutes + travelTimeMinutes
+
+    // Перевод общего количество минут в часы и минуты прибытия
+    val arrivalHour = arrivalTimeMinutes / MINUTES_IN_HOUR
+    val arrivalMinute = arrivalTimeMinutes % MINUTES_IN_HOUR
+
+    println("Время начала пути: $departureHour:$departureMinute")
+    println("Время в пути: ${travelTimeMinutes / MINUTES_IN_HOUR}:${travelTimeMinutes % MINUTES_IN_HOUR}")
+    println("Поезд прибудет в %02d:%02d".format(arrivalHour, arrivalMinute))
+}

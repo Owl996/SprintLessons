3084fb07008af3c4d9f93213dad41bed020f6e01
dlironov <dlironov@bk.ru>
2025-06-06 15:22:59 +0300

    KS-3-2-strings created

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,17 @@
+package lesson_3
+
+fun main() {
+
+    val firstName = "Татьяна"
+    var lastName = "Андреева"
+    val surName = "Сергеевна"
+    var age = 20
+
+    println("Имя: $firstName, Фамилия: $lastName, Отчество: $surName, Возраст: $age ")
+
+    lastName = "Сидорова"
+    age = 22
+
+    println("Имя: $firstName, Фамилия: $lastName, Отчество: $surName, Возраст: $age ")
+
+}

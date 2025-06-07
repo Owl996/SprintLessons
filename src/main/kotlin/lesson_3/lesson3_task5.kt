375724d72221ba8cda80351e573fbec95d7eb354
dlironov <dlironov@bk.ru>
2025-06-07 17:26:35 +0300

    KS-3-5-refactor

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -4,13 +4,11 @@ fun main() {
 
     val messageString = "D2-D4;0"
 
-    val splitMessage = messageString.split(";")
-    val moveParts = splitMessage[0]
-    val moveNumberParts = splitMessage[1]
+    val splitMessage = messageString.split("-", ";")
 
-    val splitMoveParts = moveParts.split("-")
-    val from = splitMoveParts[0]
-    val to = splitMoveParts[1]
+    val from = splitMessage[0]
+    val to = splitMessage[1]
+    val moveNumberParts = splitMessage[2]
 
     println("Откуда: $from")
     println("Куда: $to")

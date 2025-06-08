228b7bc2ee560a02c435ab2ce9d7a0707355b91b
dlironov <dlironov@bk.ru>
2025-06-08 23:14:54 +0300

    KS-5-1-if-else-when created

---
+++ src/main/kotlin/lesson_5/lesson_5.kt
@@ -0,0 +1,20 @@
+package lesson_5
+
+fun main() {
+
+    println("Нам нужно проверить что вы не робот , пожалуйста , решите следующий пример: ")
+
+    val firstNumber = (1..9).random()
+    val secondNumber = (1..9).random()
+
+    println("$firstNumber + $secondNumber")
+
+    val userInput = readln().toIntOrNull()
+
+    if (userInput == firstNumber + secondNumber) {
+        println("Добро пожаловать!")
+    } else
+        println("Доступ запрещен.")
+
+
+}

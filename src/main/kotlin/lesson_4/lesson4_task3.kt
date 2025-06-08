35650e24a4bc9110bbbe4811dad25c9136bc9087
dlironov <dlironov@bk.ru>
2025-06-08 14:06:53 +0300

    KS-4-3-operators refactor

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,18 +1,20 @@
 package lesson_4
 
+const val IS_TRUE = true
+const val IS_FALSE = false
+const val Favorable_PERCENT_AIR_HUMIDITY = 20
+const val WINTER_SEASON = "Зима"
+
 fun main() {
 
-    val sunnyWeather = true
-    val awning = true
-    val airHumidity = 20
-    val currentSeason = "Зима"
+    val isSunny = IS_TRUE
+    val isAwningOpen = IS_FALSE
+    val airHumidity = Favorable_PERCENT_AIR_HUMIDITY
+    val currentSeason = WINTER_SEASON
 
     println("[Благоприятные ли условия сейчас для роста бобовых? " +
-            (sunnyWeather && awning && airHumidity == 20 && (currentSeason != "Зима")))
-
-
-
-
+            (isSunny && isAwningOpen && airHumidity == Favorable_PERCENT_AIR_HUMIDITY
+                    && (currentSeason != WINTER_SEASON)))
 }
 
 

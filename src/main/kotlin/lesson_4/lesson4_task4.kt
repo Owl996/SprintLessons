ca0b1be362641ae3f9901c6498d7e1d84faeefb0
dlironov <dlironov@bk.ru>
2025-06-08 13:55:42 +0300

    KS-4-4-operators refactor

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -1,18 +1,21 @@
 package lesson_4
 
+const val EVEN_DAY: Int = 2
+const val REMAINDER_ZERO: Int = 0
+
 fun main() {
 
     val currentTrainingDay = 5
 
-    val armsAndAbs = (currentTrainingDay % 2) != 0
-    val legsAndBack = (currentTrainingDay % 2) == 0
+    val isArmsAndAbsDay = (currentTrainingDay % EVEN_DAY) != REMAINDER_ZERO
+    val isLegsAndBackDay = (currentTrainingDay % EVEN_DAY) == REMAINDER_ZERO
 
     println(
         """
-        Упражнения для рук:       $armsAndAbs
-        Упражнения для ног:       $legsAndBack
-        Упражнения для спины:     $legsAndBack
-        Упражнения для пресса:    $armsAndAbs
+        Упражнения для рук:       $isArmsAndAbsDay
+        Упражнения для ног:       $isLegsAndBackDay
+        Упражнения для спины:     $isLegsAndBackDay
+        Упражнения для пресса:    $isArmsAndAbsDay
     """.trimIndent()
     )
 }
